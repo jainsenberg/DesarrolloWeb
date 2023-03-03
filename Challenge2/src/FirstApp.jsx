cd19a50288a6d86e0ddbaa8fee4c0156bc12bf17
jainsenberg <110469451+jainsenberg@users.noreply.github.com>
2023-03-03 10:49:16 -0500

    Challenge2

---
+++ Challenge2/src/FirstApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const titulooo ='SOOOOOY FRANCHESCOOOO'
+
+const FirstApp = () => {
+  return (
+    <>
+    <h1>{titulooo}</h1>
+    <span>10</span>
+    </>
+  )
+}
+
+export default FirstApp
